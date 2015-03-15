3cbdf03f6a7629e924091bb9fd4fe15d128b15c1
听明 <weiqiao.cwq@tmall.com>
2015-03-15 11:43:31 +0800

    add

--- src/loadimg.js
+++ src/loadimg.js
@@ -4,24 +4,29 @@
  */
 (function ($) {
     var win = window,
-        interval = 100;//时间间隔
-    if( !$.isQmik ){
+        interval = 50;//时间间隔
+    if (!$.isQmik) {
         console.error("没有引入依赖框架Qmik,请引入Qmik2.1以上的版本")
     }
-    if(parseFloat($.version) < 2.1){
+    if (parseFloat($.version) < 2.1) {
         console.error("依赖Qmik版本2.1")
     }
     function initScroll() {
         var loadIndex = 0;
         var prevTime = new Date().getTime();
-        $(win).on('scroll', function () {
+
+        function handle(e) {
             var curTime = new Date().getTime();
             if (curTime - prevTime < interval) {
                 return;
             }
+            prevTime = curTime;
+            noCheckLoad();
+        }
+        function noCheckLoad(){
             var oldIndex = loadIndex;
             loadIndex++;
-            prevTime = curTime;
+
             var lazys = document.querySelectorAll('img.lazy');
             var length = lazys.length;
             for (var i = 0; i < length; i++) {
@@ -33,15 +38,21 @@
                     load(zlazy);
                 }
             }
-        });
+        }
+        $(win).on({
+            scroll: handle,
+            viewport: noCheckLoad
+        })
     }
 
 
     function load(mimg) {
         if (mimg.attr('loaded')) return;
         var img = new Image(),
-            url = mimg.attr('_src');
-
+            url = $.trim(mimg.attr('_src'));
+        if (!/^\w+/.test(url)) {
+            return
+        }
         img.onload = function () {
             mimg.attr('src', url).rmClass('lazy').rmClass("loading").rmAttr("_src");
             delete img;
@@ -61,11 +72,24 @@
             }, interval + 10);
         }
     };
+    $(win).on({
+        DOMNodeInserted: function (e) {
+            var dom = e.target;
+            if (dom.tagName == "IMG") {
+                var zlazy = $(dom);
+                if (zlazy.hasClass("lazy") && zlazy.inViewport()) {
+                    load(zlazy);
+                }
+            }
+        }
+    });
+    $(function () {
+        initScroll();
+        Load.load();
+        $.delay(Load.load, 1600);
+    });
 
-    initScroll();
-    Load.load();
-
-    $.define("lib/qmik/loadimg", function (require, exports, module) {
+    $.define("qmik/loadimg", function (require, exports, module) {
         module.exports = Load;
     });
 })(Qmik);
